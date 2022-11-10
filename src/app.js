6fe600c9add25843f3154a4ed738f60388438d11
pepemiguelx2 <pepemiguelx2@gmail.com>
2022-11-10 19:09:28 +0000

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.addEventListener("load", function() {
+  let excuse = "";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  excuse =
+    who[whoIndex] +
+    " " +
+    what[whatIndex] +
+    " my homework " +
+    when[whenIndex] +
+    ".";
+
+  let aux = document.querySelector("#excuse");
+  aux.innerHTML = excuse;
+});
